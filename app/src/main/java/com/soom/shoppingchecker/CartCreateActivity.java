820bc3fcd1d928a0470f4e2ff80f8a77eaf2df69
Kevin Hwang <hjs6877@gmail.com>
2017-02-21 09:34:27 +0900

    카트 추가 로직 작성 중: 신규 생성한 카트를 DB에 저장하고 닫는 부분까지 작업 완료

--- app/src/main/java/com/soom/shoppingchecker/CartCreateActivity.java
+++ app/src/main/java/com/soom/shoppingchecker/CartCreateActivity.java
@@ -64,8 +64,8 @@ public class CartCreateActivity extends AppCompatActivity {
              * - max cartId를 가져온다.
              * - new cartId = 조회한 max cartId + 1
              * - 저장.
-             * - 저장되었는지 확인 테스트 TODO
-             * - 팝업 액티비티 닫기 TODO
+             * - 저장되었는지 확인 테스트
+             * - 팝업 액티비티 닫기
              * - CartItemListAdapter를 통해 저장된 Cart에 해당하는 cart Item을 listviw에 가져와서 표시. TODO
              *      - 액티비티를 닫을 때, cartId를 응답으로 전달
              *      - 응답으로 전달 받은 cartId에 해당하는 Cart Item을 조회하여 adapter를 갱신. 기존 arrayList를 clear하고 갱신한다.
